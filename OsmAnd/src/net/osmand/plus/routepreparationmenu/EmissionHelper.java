8505f6e173eaed50444c8a3819dc58002dc0ca83
Max Kojin <nnngrach@gmail.com>
2026-07-01 15:53:14 +0500

    update fuelConsumption from new spritmonitor.de data

--- OsmAnd/src/net/osmand/plus/routepreparationmenu/EmissionHelper.java
+++ OsmAnd/src/net/osmand/plus/routepreparationmenu/EmissionHelper.java
@@ -58,12 +58,12 @@ public class EmissionHelper {
 	}
 
 	public enum MotorType {
-		PETROL(7.85f, 2.80f), // L
-		DIESEL(6.59f, 3.17f), // L
-		LPG(10.60f, 1.86f), // L
-		GAS(4.90f, 2.28f), // kg
-		ELECTRIC(21.1f, 0.42f), // kWh fuelEmissionFactor "UE except France"
-		ETHANOL(7.85f, 1.68f), // L, E85
+		PETROL(7.30f, 2.80f), // L //Normal Gasoline
+		DIESEL(6.61f, 3.17f), // L
+		LPG(10.50f, 1.86f), // L
+		GAS(4.73f, 2.28f), // kg //CNG
+		ELECTRIC(19.02f, 0.42f), // kWh fuelEmissionFactor "UE except France"
+		ETHANOL(7.85f, 1.68f), // L, Biolcohol (E85)
 		HYBRID(5.61f, 2.80f); // L, hybrid petrol
 
 		public final float fuelConsumption; // unit (L/kwH/kg)/100km
